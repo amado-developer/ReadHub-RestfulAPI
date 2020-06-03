ead839c282ccea173e9bd8d1d5e1394ef63f53d9
amado-developer <amado_developer@hotmail.com>
2020-06-03 09:01:22 -0600

    changes

--- ReadHub/settings.py
+++ ReadHub/settings.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
 
 JWT_AUTH = {
 'JWT_ALLOW_REFRESH' : True,
-'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=24),
+'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=48),
 'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
 }
 
